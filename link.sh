19a2ee852e9db324f6d4df8989fa9b2267834451
kasemm <mohamed.kasem@gig.tech>
2020-10-05 23:10:21 +0200

    Create a linking script

--- link.sh
+++ link.sh
@@ -1,5 +1,2 @@
 #! /bin/bash
-sudo ln -s $(pwd)/regolith-i3-config /etc/regolith/i3/config -f
-ln -s $(pwd)/vimrc $HOME/.vimrc -f
-ln -s $(pwd)/bashrc $HOME/.bashrc -f
-ln -s $(pwd)/zshrc $HOME/.zshrc -f
+exec sudo -E ./link.py
