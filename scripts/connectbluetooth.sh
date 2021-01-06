68808fb7574676f84ebf2b83c939784435027c8b
kasemm <mohamed.kasem@gig.tech>
2021-01-07 00:16:06 +0200

    Update configs

--- scripts/connectbluetooth.sh
+++ scripts/connectbluetooth.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-DEVICE=$(bluetoothctl paired-devices | grep Device |awk '$1=""; {print $0}' | dmenu) 
+DEVICE=$(bluetoothctl paired-devices | grep Device |awk '$1=""; {print $0}'| sort -r | dmenu) 
 MAC=$(echo $DEVICE | awk '{print $1}')
 
 bluetoothctl disconnect $MAC 
 bluetoothctl connect $MAC 
-
