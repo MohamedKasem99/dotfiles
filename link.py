4a1e3a0c4caa3271da895a047958642c26c5df25
kasemm <mohamed.kasem@gig.tech>
2021-01-17 18:13:03 +0200

    Improve link.py

--- link.py
+++ link.py
@@ -4,6 +4,7 @@ import sys
 
 HOME = os.environ.get("HOME")
 if not HOME:
+    print("HOME env variable is not set. Exitting")
     sys.exit(1)
 
 FILE_GUIDE = {
@@ -12,26 +13,6 @@ FILE_GUIDE = {
     "zshrc": "{}/.zshrc".format(HOME),
     "regolith-i3-config": "{}/.config/regolith/i3/config".format(HOME),
 }
-
-IGNORED = [
-    "link.py",
-    ".git",
-    "scripts",
-    "dotfiles",
-    "installer.sh",
-]
-
-
-def display_warnings():
-    files_not_included = filter(
-        lambda x: x not in list(FILE_GUIDE.keys()) + IGNORED, os.listdir()
-    )
-    for not_included in files_not_included:
-        print(
-            "[!!!] File: {} is in the repo but has no specified destination".format(
-                not_included
-            )
-        )
 
 
 def check_target_path(target):
@@ -43,7 +24,6 @@ def check_target_path(target):
 
 
 def main():
-    display_warnings()
     for src, dest in FILE_GUIDE.items():
         src_path = f"{HOME}/dotfiles/dotfiles/{src}"
         if os.path.exists(src_path):
